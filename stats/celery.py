12a337f44ed8aee4a69af8806e33f5136b8b21fa
Li Lin <guru.lin@gmail.com>
2020-10-14 02:29:34 +0800

    added scheduled job

--- stats/celery.py
+++ stats/celery.py
@@ -1,5 +1,8 @@
 import os
+
 from celery import Celery
+from celery.schedules import crontab
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crawler.settings")
 app = Celery("stats")
@@ -14,3 +17,19 @@ app.autodiscover_tasks()
 def debug_task(self):
     print(f"Request: {self.request!r}")
     return "OK"
+
+
+@app.task(bind=True)
+def discover_git_projects_task(self):
+    from stats import indexer
+    from stats.indexer import register_git_projects
+    from stats.models import ConfigEntry
+    conf = ConfigEntry.get(indexer.DEFAULT_CONFIG)
+    register_git_projects(conf)
+
+
+@app.on_after_finalize.connect
+def setup_periodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(
+        crontab(hour="*", minute="*/2", day_of_week="*"), discover_git_projects_task.s()
+    )
