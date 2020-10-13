f9bf34755a17fce3b48cd028772ab7398538a30f
Li Lin <guru.lin@gmail.com>
2020-10-13 17:03:16 +0800

    fixes issues with tests

--- repos/views.py
+++ repos/views.py
@@ -1,15 +1,10 @@
 from django.http import JsonResponse
 from .models import Author
-from .celery import debug_task
 
 
 def index(request):
-    task = debug_task.delay()
-    result = task.wait()
-
     authors = Author.objects.all()
     response = {
         "authors": [{"name": a.name, "email": a.email} for a in authors],
-        "task": result
     }
     return JsonResponse(response)
