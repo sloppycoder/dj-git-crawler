8c20b28e8a3edf79d0900fc9688281a03b392ebb
Li Lin <guru.lin@gmail.com>
2020-10-13 12:39:42 +0800

    # create tables for admin user python manage.py migrate
    # update model class then create migration file
    # then run migration to create model
    python manage.py makemigrations repos
    python manage.py sqlmigrate repos 0001
    python manage.py migrate
    
    # then create superuser
    python manage.py createsuperuser

--- repos/models.py
+++ repos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=64, null=False)
+    email = models.CharField(max_length=64, null=False)
+    tag1 = models.CharField(max_length=16)
+    tag2 = models.CharField(max_length=16)
+    tag3 = models.CharField(max_length=16)
+    is_alias = models.BooleanField(default=False)
