5652e0c1c739976f509591ebd0830d95380b1cd1
Li Lin <guru.lin@gmail.com>
2020-10-14 16:35:47 +0800

    created jobs for indexing

--- stats/celery.py
+++ stats/celery.py
@@ -1,8 +1,9 @@
+import datetime
 import os
 
 from celery import Celery
 from celery.schedules import crontab
-
+from django.utils import timezone
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "crawler.settings")
 app = Celery("stats")
@@ -14,23 +15,42 @@ app.autodiscover_tasks()
 
 
 @app.task(bind=True)
-def debug_task(self):
-    print(f"Request: {self.request!r}")
-    return "OK"
+def discover_repositories_task(self):
+    # import any of these at the beginning of the file will
+    # get an "Apps aren't loaded yet" error
+    from stats.indexer import register_git_repositories
+
+    register_git_repositories()
 
 
 @app.task(bind=True)
-def discover_git_projects_task(self):
-    from stats import indexer
-    from stats.indexer import register_git_projects
-    from stats.models import ConfigEntry
+def index_repository_task(self, **kwargs):
+    # import any of these at the beginning of the file will
+    # get an "Apps aren't loaded yet" error
+    from stats.indexer import index_repository
 
-    conf = ConfigEntry.get(indexer.DEFAULT_CONFIG)
-    register_git_projects(conf)
+    repo_id = kwargs["repo_id"]
+    index_repository(repo_id)
+
+
+@app.task(bind=True)
+def index_all_repositories_task(self):
+    # import any of these at the beginning of the file will
+    # get an "Apps aren't loaded yet" error
+    from stats.indexer import scan_repositories
+
+    cut_off = timezone.make_aware(datetime.now() - datetime.timedelta(minutes=5))
+    for repo in scan_repositories(cut_off=cut_off):
+        index_repository_task(repo_id=repo.id).delay()
 
 
 @app.on_after_finalize.connect
 def setup_periodic_tasks(sender, **kwargs):
     sender.add_periodic_task(
-        crontab(hour="*", minute="*/2", day_of_week="*"), discover_git_projects_task.s()
+        crontab(hour="*", minute="*/2", day_of_week="*"), discover_repositories_task.s()
+    )
+
+    sender.add_periodic_task(
+        crontab(hour="*", minute="*/10", day_of_week="*"),
+        index_all_repositories_task.s(),
     )
