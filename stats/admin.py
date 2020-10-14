e9adde13c54a92b73ab9f2e92dc517a2e0f87481
Li Lin <guru.lin@gmail.com>
2020-10-14 19:00:59 +0800

    register models for admin site

--- stats/admin.py
+++ stats/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import Author
+from .models import Author, ConfigEntry, Repository, Commit
 
 admin.site.register(Author)
+admin.site.register(ConfigEntry)
+admin.site.register(Repository)
+admin.site.register(Commit)
