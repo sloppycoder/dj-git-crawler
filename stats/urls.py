8f11a3c5218e8ad674e68bb40e391908391670c9
Li Lin <guru.lin@gmail.com>
2020-10-16 01:35:03 +0800

    added endpoint to trigger jobs manually

--- stats/urls.py
+++ stats/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("discover", views.discover, name="discover"),
+    path("scan", views.scan, name="scan"),
 ]
--- stats/views.py
+++ stats/views.py
@@ -1,5 +1,6 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from .models import Author
+from .celery import discover_repositories_task, index_all_repositories_task
 
 
 def index(request):
@@ -8,3 +9,21 @@ def index(request):
         "authors": [{"name": a.name, "email": a.email} for a in authors],
     }
     return JsonResponse(response)
+
+
+def discover(request):
+    code = request.GET.get("code", "")
+    if code == "s3cr3t":
+        discover_repositories_task.delay()
+        return JsonResponse({"status": "Submitted"})
+    else:
+        return HttpResponse("Unauthorized", status=401)
+
+
+def scan(request):
+    code = request.GET.get("code", "")
+    if code == "s3cr3t":
+        index_all_repositories_task.delay()
+        return JsonResponse({"status": "Submitted"})
+    else:
+        return HttpResponse("Unauthorized", status=401)
