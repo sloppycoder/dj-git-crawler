86afc6005ffe947cbd7438c3ccafd6fd9c1662ea
Li Lin <guru.lin@gmail.com>
2020-10-17 02:05:08 +0800

    create admin user without prompting for password. make it easier to run in non-interactive mode

--- stats/management/commands/initapp.py
+++ stats/management/commands/initapp.py
@@ -1,24 +1,22 @@
-import getpass
-from django.core.management.base import BaseCommand, CommandError
+import os
+
+from django.core.management.base import BaseCommand
 from stats.models import ConfigEntry
 from django.db.utils import IntegrityError
 from django.contrib.auth import get_user_model
 
 
 class Command(BaseCommand):
-    help = "Iniitlize the application by load ini file into database and create users"
+    help = "Initialize the application by load ini file into database and create users"
 
     def handle(self, *args, **options):
-        ConfigEntry.load("crawler.ini", "crawler/crawler.ini")
+        ini_file = os.getenv("CRAWLER_INI", "crawler/crawler.ini")
+        ConfigEntry.load("crawler.ini", ini_file)
         assert ConfigEntry.get("crawler.ini") is not None
+        print(f"{ini_file} loaded into database")
 
         self.create_user("user", "user")
-
-        password = getpass.getpass()
-        repeat = getpass.getpass(prompt="Password again:")
-        if password != repeat:
-            raise CommandError("Passwords entered does not match")
-        self.create_user("admin", password)
+        self.create_user("admin", "admin")
 
     def create_user(self, username, password):
         user = get_user_model()
