eb014835f5a3f263b17d1661912ec909ddc9a73b
Li Lin <guru.lin@gmail.com>
2020-10-16 15:37:29 +0800

    added redirect

--- crawler/urls.py
+++ crawler/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="admin/", permanent=False)),
     path("stats/", include("stats.urls")),
     path("admin/", admin.site.urls),
-    path("", RedirectView.as_view(url=admin.site.urls)),
 ]
