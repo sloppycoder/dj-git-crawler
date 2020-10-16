2a41b94aa31c68fa0abfa362cca4528647e7c23d
Li Lin <guru.lin@gmail.com>
2020-10-17 02:06:18 +0800

    restore route test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,1 +1,9 @@
-import pytest
+def test_stats_endpoints(client):
+    response = client.get("/stats/scan")
+    assert response.status_code == 401
+
+    response = client.get("/stats/discover")
+    assert response.status_code == 401
+
+    response = client.get("/stats/stat")
+    assert response.status_code == 401
